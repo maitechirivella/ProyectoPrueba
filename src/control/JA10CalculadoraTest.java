542aac36d7338af0df9229b55a026600dccc57c0
usuario <usuario@E1-571.mshome.net>
2016-05-31 23:27:32 +0200

    #1 -> Inicio del proyecto

---
+++ src/control/JA10CalculadoraTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+import grafic.VentanaCalculadora;
+
+/**
+ *
+ * @author usuario
+ */
+public class JA10CalculadoraTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaCalculadora calculadora = new VentanaCalculadora();
+        calculadora.setVisible(true);
+    }
+
+}
